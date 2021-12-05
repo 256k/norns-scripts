3b75615538bc87787571dfac0b65ce3639537182
256k <fayek.helmi@gmail.com>
2021-12-04 18:28:57 -0600

    WIP added screen brightness knob

--- hello_norns/screenplay.lua
+++ hello_norns/screenplay.lua
@@ -8,10 +8,11 @@
 
 
 function init() 
-  myChar = 33
+  myChar = 47
+  charLight = 1
   clock.run(function()  -- redraw the screen and grid at 15fps
     while true do
-      clock.sleep(1/5)
+      clock.sleep(1/15)
       redraw()
       -- gridredraw() -- for the grid
     end
@@ -25,13 +26,22 @@ function key(n,z)
   end
   if n==3 and z==1 then
     myChar = math.random(33, 126)
-    
+    -- charLight = math.random(1, 15)
+    -- print(charLight)
   end
   
 end
 
+function enc(n,d)
+  if n==3 then
+    if charLight > 0 and charLight < 16 then
+    charLight = charLight + d
+    end
+end
+end
+
 function redraw()
-  
+    screen.level(charLight)
     screen.move(math.random(0, 128), math.random(0, 64))
     screen.text(string.char(myChar))
     screen.update()
